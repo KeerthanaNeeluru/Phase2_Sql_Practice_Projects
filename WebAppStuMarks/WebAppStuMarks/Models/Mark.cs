73776ca5fae321e886e5c30c65ef66ebfc2e9be7
keerthana <keerthananeeluru2001@gmail.com>
2023-09-07 14:24:20 +0530

    adding 3rd practice project

---
+++ WebAppStuMarks/WebAppStuMarks/Models/Mark.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAppStuMarks.Models
+{
+    [Table("Mark")]
+    public class Mark
+    {
+        [Key]
+        public int Id { get; set; }
+        public string SubName { get; set; }
+        public double Marks { get; set; }
+        public int SId { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
